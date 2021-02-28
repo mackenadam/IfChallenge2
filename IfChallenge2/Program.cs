738fd9d081255d61c3c1541131290767c218bebb
Adam Macken <mackenadam@gmail.com>
2021-02-28 15:07:48 +1300

    Add project files.

---
+++ IfChallenge2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/* Create an application with a score, highscore and a highscorePlayer.
+ Create a method which has two parameters, one for the score and one for the playerName.
+When ever that method is called, it should be checked if the score of the player is higher than the highscore, if so, "New highscore is + " score" and in another line "New highscore holder is " + playerName - should be written onto the console, if not "The old highscore of " + highscore + " could not be broken and is still held by " + highscorePlayer.
+Consider which variables are required globally and which ones locally. */
+
+namespace IfChallenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+        }
+    }
+}
